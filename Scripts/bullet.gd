84534bd450e34dfa745aeea1b3c85993ab6e3062
ArrowTheFighter <30394494+ArrowTheFighter@users.noreply.github.com>
2025-02-04 21:59:57 +0100

    Updated bullet script to hurt "enemies"

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -10,3 +10,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	move_local_x(speed * delta)
 	pass
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if(area.get_parent().is_in_group("Enemy")):
+		area.get_parent().queue_free()
+		queue_free()
+	pass # Replace with function body.
