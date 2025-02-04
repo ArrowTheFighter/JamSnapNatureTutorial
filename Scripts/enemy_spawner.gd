cc4675891b7685e02438dd26b9c42f17b9b6edf0
ArrowTheFighter <30394494+ArrowTheFighter@users.noreply.github.com>
2025-02-04 22:01:41 +0100

    adjusted enemy spawner

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var timer: Timer = $Timer
+
 @export var enemy_scene: Resource
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,3 +21,5 @@ func spawn_enemy():
 	var instance = enemy_scene.instantiate()
 	get_parent().add_child(instance)
 	instance.global_position = global_position
+	timer.wait_time = timer.wait_time * 0.98
+	
