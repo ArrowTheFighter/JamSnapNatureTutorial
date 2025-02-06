9c99c4fafb8496479b13a99609201c66341f06ae
ArrowTheFighter <30394494+ArrowTheFighter@users.noreply.github.com>
2025-02-06 14:22:20 +0100

    Changed bullet to deal a specified amount of damage

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,19 +1,15 @@
 extends Node2D
 
 @export var speed := 100
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
+@export var damage := 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	move_local_x(speed * delta)
 	pass
 
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if(area.get_parent().is_in_group("Enemy")):
-		area.get_parent().queue_free()
+		var enemy = area.get_parent() as Enemy
+		enemy.take_damage(damage)
 		queue_free()
 	pass # Replace with function body.
