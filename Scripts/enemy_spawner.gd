61cd5aa653d6dde496ef2dc9d6b8db0158b11964
ArrowTheFighter <30394494+ArrowTheFighter@users.noreply.github.com>
2025-02-06 14:22:50 +0100

    Added random height to spawner

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -1,25 +1,13 @@
+class_name Spawner
 extends Node2D
 
 @onready var timer: Timer = $Timer
 
 @export var enemy_scene: Resource
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_timer_timeout() -> void:
-	spawn_enemy()
-	pass # Replace with function body.
 
 func spawn_enemy():
 	var instance = enemy_scene.instantiate()
 	get_parent().add_child(instance)
-	instance.global_position = global_position
+	instance.global_position = global_position + Vector2.UP * (randf_range(-1,1) * 30)
 	timer.wait_time = timer.wait_time * 0.98
 	
